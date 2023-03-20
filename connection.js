5248f3e8433dcdcf3f854e8369445e080bf0c8bb
Anna Serobyan <anna.serobyan97@gmail.com>
2023-03-20 21:58:51 +0400

    second commit

--- connection.js
+++ connection.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2');
 require('dotenv').config();
 
 const pool = mysql.createPool({
@@ -11,46 +11,53 @@ const pool = mysql.createPool({
   queueLimit: 0
 });
 
-(async () => {
-  try {
-    const usersTableQuery = `
-      CREATE TABLE IF NOT EXISTS users (
-        id INT PRIMARY KEY AUTO_INCREMENT,
-        name VARCHAR(255) NOT NULL,
-        email VARCHAR(255) NOT NULL UNIQUE,
-        password VARCHAR(255) NOT NULL,
-        isAdmin BOOLEAN NOT NULL DEFAULT false
-      )
-    `;
-    await pool.query(usersTableQuery);
-    console.log('Users table created or already exists');
-
-    const productsTableQuery = `
-      CREATE TABLE IF NOT EXISTS products (
-        id INT PRIMARY KEY AUTO_INCREMENT,
-        name VARCHAR(255) NOT NULL,
-        price DECIMAL(10,2) NOT NULL,
-        description TEXT NOT NULL
-      )
-    `;
-    await pool.query(productsTableQuery);
-    console.log('Products table created or already exists');
-
-    const cartItemsTableQuery = `
-      CREATE TABLE IF NOT EXISTS cartItems (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        user_id INT NOT NULL,
-        product_id INT NOT NULL,
-        quantity INT NOT NULL,
-        FOREIGN KEY (user_id) REFERENCES users(id),
-        FOREIGN KEY (product_id) REFERENCES products(id)
-      )
-    `;
-    await pool.query(cartItemsTableQuery);
-    console.log('CartItems table created or already exists');
-  } catch (error) {
+pool.query(`
+  CREATE TABLE IF NOT EXISTS users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    isAdmin BOOLEAN NOT NULL DEFAULT false
+  )
+`, (error, results, fields) => {
+  if (error) {
     console.error(error);
+  } else {
+    console.log('Users table created or already exists');
   }
-})();
+});
+pool.query(`
+ 
+CREATE TABLE IF NOT EXISTS products (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  price DECIMAL(10,2) NOT NULL,
+  description TEXT NOT NULL
+)
+`, (error, results, fields) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log('products table created or already exists');
+  }
+});
+pool.query(`
+ 
+CREATE TABLE IF NOT EXISTS cartItems (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  user_id INT NOT NULL,
+  product_id INT NOT NULL,
+  quantity INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (product_id) REFERENCES products(id)
+)
+`, (error, results, fields) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log('cartItems table created or already exists');
+  }
+});
 
-module.exports = pool.promise;
+
+module.exports = pool;
--- routes/users.js
+++ routes/users.js
@@ -3,28 +3,28 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const pool = require('../connection');
 require('dotenv').config();
-// const authenticateAdmin = require('../middlewares/auth');
+const authenticateAdmin  = require('../middlewares/auth');
 
 const router = express.Router();
+
 const secretKey = process.env.secretKey;
+
+
+
 
 router.post('/', (req, res) => {
   const { email, password } = req.body;
 
-  new Promise((resolve, reject) => {
-    pool.query('SELECT * FROM users WHERE email = ?', [email], (err, rows) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(rows);
-      }
-    });
-  }).then((rows) => {
+  pool.query('SELECT * FROM users WHERE email = ?', [email], (err, rows) => {
+    if (err) throw err;
+
     if (rows.length === 0) {
       return res.status(401).json({ message: 'Invalid email or password' });
     }
 
     const user = rows[0];
+
+    // compare the password entered by the user with the hashed password stored in the database
     const passwordMatch = bcrypt.compareSync(password, user.password);
 
     if (!passwordMatch) {
@@ -32,13 +32,10 @@ router.post('/', (req, res) => {
     }
 
     const token = jwt.sign({ email: user.email, isAdmin: Boolean(user.isAdmin) }, secretKey, { expiresIn: '1h' });
+
     return res.json({ message: 'Authentication successful', token });
-  }).catch((err) => {
-    console.error(err);
-    return res.status(500).json({ message: 'Server error' });
   });
 });
 
 module.exports = router;
-
 

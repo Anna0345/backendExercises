b2cb58b846f379e0e63bc76a9653d8ce6e4e1b49
Anna Serobyan <anna.serobyan97@gmail.com>
2023-03-20 22:47:45 +0400

    Revert "second commit"
    This reverts commit 5248f3e8433dcdcf3f854e8369445e080bf0c8bb.

--- routes/products.js
+++ routes/products.js
@@ -1,57 +1,70 @@
+
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const pool = require('../connection');
-const authenticateAdmin  = require('../middlewares/auth');
+const authenticateAdmin = require('../middlewares/auth');
 require('dotenv').config();
 
 const router = express.Router();
 
-
 // Routes that require authentication and authorization
 router.get('/', (req, res) => {
-  pool.query('SELECT * FROM products', (err, rows) => {
-    if (err) throw err;
-    res.json(rows);
-  });
+ pool.query('SELECT * FROM products')
+  .then(rows => {
+  res.json(rows);
+  })
+.catch(err => {
+throw err;
+});
 });
 
 router.get('/:id', (req, res) => {
-  const { id } = req.params;
-  const query = 'SELECT * FROM products WHERE id = ?';
-  pool.query(query, [id], (err, rows) => {
-    if (err) throw err;
+const { id } = req.params;
+const query = 'SELECT * FROM products WHERE id = ?';
+pool.query(query, [id])
+   .then(rows => {
     res.json(rows);
-  });
+})
+.catch(err => {
+throw err;
+});
 });
 
-
-
 router.post('/', authenticateAdmin, (req, res) => {
-  const { name, price, description } = req.body;
-  const query = 'INSERT INTO products (name, price, description) VALUES (?, ?, ?)';
-  pool.query(query, [name, price, description], (err, result) => {
-    if (err) throw err;
-    res.json({ message: 'Product added successfully', productId: result.insertId });
-  });
+const { name, price, description } = req.body;
+const query = 'INSERT INTO products (name, price, description) VALUES (?, ?, ?)';
+pool.query(query, [name, price, description])
+  .then(result => {
+   res.json({ message: 'Product added successfully', productId: result.insertId });
+})
+  .catch(err => {
+  throw err;
+});
 });
 
 router.put('/:id', authenticateAdmin, (req, res) => {
-  const { name, price, description } = req.body;
-  const { id } = req.params;
-  const query = 'UPDATE products SET name = ?, price = ?, description = ? WHERE id = ?';
-  pool.query(query, [name, price, description, id], (err, result) => {
-    if (err) throw err;
-    res.json({ message: `Product ${id} updated successfully` });
-  });
+const { name, price, description } = req.body;
+const { id } = req.params;
+const query = 'UPDATE products SET name = ?, price = ?, description = ? WHERE id = ?';
+pool.query(query, [name, price, description, id])
+.then(result => {
+res.json({ message: `Product ${id} updated successfully` });
+})
+.catch(err => {
+throw err;
+});
 });
 
 router.delete('/:id', authenticateAdmin, (req, res) => {
-  const { id } = req.params;
-  const query = 'DELETE FROM products WHERE id = ?';
-  pool.query(query, [id], (err, result) => {
-    if (err) throw err;
-    res.json({ message: `Product ${id} deleted successfully` });
-  });
+const { id } = req.params;
+const query = 'DELETE FROM products WHERE id = ?';
+pool.query(query, [id])
+.then(result => {
+res.json({ message:` Product ${id} deleted successfully `});
+})
+.catch(err => {
+throw err;
+});
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,28 +3,28 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const pool = require('../connection');
 require('dotenv').config();
-const authenticateAdmin  = require('../middlewares/auth');
+// const authenticateAdmin = require('../middlewares/auth');
 
 const router = express.Router();
-
 const secretKey = process.env.secretKey;
-
-
-
 
 router.post('/', (req, res) => {
   const { email, password } = req.body;
 
-  pool.query('SELECT * FROM users WHERE email = ?', [email], (err, rows) => {
-    if (err) throw err;
-
+  new Promise((resolve, reject) => {
+    pool.query('SELECT * FROM users WHERE email = ?', [email], (err, rows) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(rows);
+      }
+    });
+  }).then((rows) => {
     if (rows.length === 0) {
       return res.status(401).json({ message: 'Invalid email or password' });
     }
 
     const user = rows[0];
-
-    // compare the password entered by the user with the hashed password stored in the database
     const passwordMatch = bcrypt.compareSync(password, user.password);
 
     if (!passwordMatch) {
@@ -32,10 +32,13 @@ router.post('/', (req, res) => {
     }
 
     const token = jwt.sign({ email: user.email, isAdmin: Boolean(user.isAdmin) }, secretKey, { expiresIn: '1h' });
-
     return res.json({ message: 'Authentication successful', token });
+  }).catch((err) => {
+    console.error(err);
+    return res.status(500).json({ message: 'Server error' });
   });
 });
 
 module.exports = router;
+
 
